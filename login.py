379f07d4fa2d1bda588127aab52969226fe0a4e6
krishna <krishnatiwari9191@gmail.com>
2021-04-28 17:38:40 +0200

    complete registration, delete,modify rows and authentication part

--- login.py
+++ login.py
@@ -1,7 +1,7 @@
-
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,make_response
 from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
+from functools import wraps
 from marshmallow import Schema, fields
 
 app = Flask(__name__)
@@ -27,13 +27,36 @@ class UserSchema(Schema):
     lastname = fields.Str()
 
 
+user = UserSchema()
+users = UserSchema(many=True)
+
+
+def login_decorator(func):
+    @wraps(func)
+    def check_login(*args, **kwargs):
+        auth = request.authorization
+        if auth.username == User.username and auth.password == User.password:
+           return func(*args, **kwargs)
+        else:
+            return make_response('you are not allowed to visit this page',401)
+    return check_login
+
+
 class UserInfo(Resource):
 
     def get(self):
         users = User.query.all()
-        schema = UserSchema(many=True)
-        output = schema.dump(users)
-        return output
+        results = []
+        for user in users:
+            data_user = dict()
+            data_user['username'] = user.username
+            data_user['password'] = user.password
+            data_user['email'] = user.email
+            data_user['firstname'] = user.firstname
+            data_user['lastname'] = user.lastname
+            results.append(data_user)
+
+        return jsonify({'users': results})
 
     def post(self):
         first_name = request.json['Firstname']
@@ -41,19 +64,23 @@ class UserInfo(Resource):
         email = request.json['Email']
         user_name = request.json['Username']
         password = request.json['Password']
-        user = User(username=user_name,password=password,email=email,firstname=first_name,lastname=last_name)
+        user = User(username=user_name, password=password, email=email, firstname=first_name, lastname=last_name)
         db.session.add(user)
         db.session.commit()
         return 'Successfully inserted'
+
 
 class UserOperation(Resource):
 
     def get(self, id):
         user = User.query.get(id)
-        schema = UserSchema()
-        output = schema.dump(user)
-        return output
-
+        data_user = dict()
+        data_user['username'] = user.username
+        data_user['password'] = user.password
+        data_user['email'] = user.email
+        data_user['firstname'] = user.firstname
+        data_user['lastname'] = user.lastname
+        return jsonify({'user': data_user})
 
     def delete(self, id):
         user = User.query.get(id)
@@ -61,9 +88,49 @@ class UserOperation(Resource):
         db.session.commit()
         return 'Successfully deleted'
 
+    def put(self, id):
+        user = User.query.get(id)
+        username = request.json.get('Username', 'None')
+        if username != 'None':
+            user.username = username
+        firstname = request.json.get('Firstname', 'None')
+        if firstname != 'None':
+            user.firstname = firstname
+        lastname = request.json.get('Lastname', 'None')
+        if lastname != 'None':
+            user.lastname = lastname
+        email = request.json.get('Email', 'None')
+        if email != 'None':
+            user.email = email
+
+        db.session.commit()
+        return 'successfully updated'
+
+
+
+@app.route('/change_password/<email>', methods=['PUT'])
+def changing_password(email):
+    user = User.query.filter_by(email=email).first()
+    password = request.json['Password']
+    user.password = password
+    db.session.commit()
+    return 'password changed '
+
+
+@app.route('/login')
+def check_login():
+    users = User.query.all()
+    auth = request.authorization
+    for user in users:
+        if auth and auth.username == user.username and auth.password == user.password:
+            return 'login successful'
+        else:
+            return 'login failed'
+
 
 api.add_resource(UserInfo, '/user')
-api.add_resource(UserOperation,'/user/<int:id>')
+api.add_resource(UserOperation, '/user/<int:id>')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
